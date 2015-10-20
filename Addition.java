654cd4f87a03d25242a408aae1e70fd175c79b5f
laiq anwar <laiqanwar44@gmail.com>
2015-10-20 22:51:30 +0500

    d0ne...

---
+++ Addition.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition;
+
+/**
+ *
+ * @author Perfect IT
+ */
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5; 
+        int b = 3;
+        
+        System.out.println("sum = " + (a+b));
+    }
+    
+}
